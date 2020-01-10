374f79372d966a5f35fbcf2a82ff16b7b5dfd117
kennansalisbury <kennansalisbury@gmail.com>
2020-01-10 13:55:12 -0800

    completed, ready to deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
+const db = require('./models')
 
 const app = express();
 
@@ -10,7 +11,52 @@ app.use(methodOverride('_method'));
 
 // WRITE YOUR ROUTES HERE /////////////////////
 
+//GET '/' - display
+app.get('/', (req, res) => {
 
+    db.widget.findAll()
+    .then(widgets => {
+        console.log('🌈🌈🌈' + widgets)
+        res.render('index', { widgets })
+    })
+    .catch(err => {
+        console.log(err)
+        res.send('ERROR')
+    })
+})
+
+
+//POST '/' - add widget
+app.post('/', (req, res) => {
+    
+    db.widget.create({
+        description: req.body.description,
+        quantity: req.body.quantity
+    })
+    .then(widget => {
+        console.log(widget + 'created 🌷🌷🌷')
+        res.redirect('/')
+    })
+    .catch(err => {
+        console.log(err)
+        res.send('ERROR')
+    })
+})
+
+//DELETE '/' - delete widget
+app.delete('/', (req, res) => {
+    
+    db.widget.destroy({
+        where: {id: req.body.id}
+    })
+    .then(destroyed => {
+        res.redirect('/')
+    })
+    .catch(err => {
+        console.log(err)
+        res.send('ERROR')
+    })
+})
 
 // YOUR ROUTES ABOVE THIS COMMENT /////////////
 
